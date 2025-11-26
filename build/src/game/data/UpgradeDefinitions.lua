ce7d5655665fa78013afab4771ddb9953ed76d4c
rodrigo.dornelles <rodrigo.dornelles.poa@gmail.com>
2025-11-25 23:41:26 -0300

    feat: upgrade to gly 0.3.X

--- build/src/game/data/UpgradeDefinitions.lua
+++
@@ -1,3 +0,0 @@
-local ____exports = {}
-____exports.UPGRADE_CARD_LIST = {{id = "combo_naipes", name = "Combo de Naipe", texture = "card1.png", special_effect = 1}, {id = "eco_inverso", name = "Eco Inverso", texture = "card2.png", special_effect = 2}, {id = "baralho_ensanguentado", name = "Baralho Ensanguentado", texture = "card3.png", special_effect = 3}}
-return ____exports
